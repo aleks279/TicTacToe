ea67cfd08501d5f098e46d0850f7650b5556d2dd
aleks279 <aleks279@gmail.com>
2014-05-20 15:16:22 -0600

    Refactored Game#play. Game#MAPPING now constant.

--- lib/TicTacToe/game.rb
+++ lib/TicTacToe/game.rb
@@ -2,62 +2,60 @@ module TicTacToe
 	require 'observer'
 	class Game
 		include Observable
-		attr_accessor :players, :board, :current_player, :second_player, :move_list, :mapping
-		def initialize(players, board = Board.new)
+		attr_accessor :players, :board, :current_player, :second_player, :move_list
+		MAPPING = { 
+				"1" => [0, 0], "2" => [1, 0], "3" => [2, 0],
+				"4" => [0, 1], "5" => [1, 1], "6" => [2, 1],
+				"7" => [0, 2], "8" => [1, 2], "9" => [2, 2] }
+
+    def initialize(players, board = Board.new)
 			add_observer(Notifier.new)
 			@players = players
 			@board = board
 			@current_player, @second_player = players.shuffle
 			@move_list = Array.new
-			@mapping = { 
-				"1" => [0, 0], "2" => [1, 0], "3" => [2, 0],
-				"4" => [0, 1], "5" => [1, 1], "6" => [2, 1],
-				"7" => [0, 2], "8" => [1, 2], "9" => [2, 2] }
-		end
+    end
 
-		def switch_players
+		def play
+			puts "#{current_player.name} is the first player"
+			next_turn until board.game_over
+      changed
+      notify_observers(current_player, board.game_over)
+      print_formatted_grid
+    end
+    
+    def next_turn
+      print_formatted_grid
+      ask_for_move
+      switch_players
+    end
+
+		private
+
+    def switch_players
 			@current_player, @second_player = @second_player, @current_player
 		end
 
 		def ask_for_move
 			puts "#{current_player.name}: Enter a number from 1 to 9 to make your move"
+      row, column = get_move
+      board.grid[column][row].value = current_player.token
 		end
 
 		def get_move(move = gets.chomp)
 			move_to_coordinate(move)
 		end
 
-		def play
-			puts "#{current_player.name} is the first player"
-			while true
-				formatted_grid
-				puts ""
-				ask_for_move
-				row,column = get_move
-				board.grid[column][row].value = current_player.token
-				if board.game_over
-					changed
-					notify_observers(current_player, board.game_over)
-					formatted_grid
-					return
-				else
-					switch_players
-				end
-			end
-		end
-
-		private
-
-		def formatted_grid
-			return board.grid.each do |row|
+		def print_formatted_grid
+			board.grid.each do |row|
 				puts row.map {|cell| cell.value.empty? ? "_" : cell.value}.join(" ")
 			end
 		end
 
 		def move_to_coordinate(move)
-			if !move_list.include?(move) and mapping.include?(move)
+			if !move_list.include?(move) and MAPPING.include?(move)
 				move_list.push(move)
-				return mapping[move]
+				return MAPPING[move]
 			else
 				puts "Error: invalid move!"
 				puts ask_for_move
